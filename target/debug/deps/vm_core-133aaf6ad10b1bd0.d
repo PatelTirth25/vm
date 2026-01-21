b3f18f7444183a4d091a6aa7fea9e81515177fe9
PatelTirth25 <tirthpatel204@gmail.com>
2026-01-21 16:57:18 +0530

    .gitignore added

--- target/debug/deps/vm_core-133aaf6ad10b1bd0.d
+++
@@ -1,7 +0,0 @@
-/home/tirth/code/vm/target/debug/deps/vm_core-133aaf6ad10b1bd0.d: vm-core/src/lib.rs
-
-/home/tirth/code/vm/target/debug/deps/libvm_core-133aaf6ad10b1bd0.rlib: vm-core/src/lib.rs
-
-/home/tirth/code/vm/target/debug/deps/libvm_core-133aaf6ad10b1bd0.rmeta: vm-core/src/lib.rs
-
-vm-core/src/lib.rs:
--- target/debug/deps/vm_host-8ad5639d9e3d6edd.d
+++
@@ -1,5 +0,0 @@
-/home/tirth/code/vm/target/debug/deps/vm_host-8ad5639d9e3d6edd.d: vm-host/src/main.rs
-
-/home/tirth/code/vm/target/debug/deps/vm_host-8ad5639d9e3d6edd: vm-host/src/main.rs
-
-vm-host/src/main.rs:
